76d71d3afe5d82ebc8d90a0c0b83c51d3901602b
Ana Perola <anaperolinha@hotmail.com>
2022-05-07 23:17:15 -0300

    Add config of email

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import { prisma } from './prisma';
 import express from 'express';
+import nodemailer from 'nodemailer';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "472b7d793bcd2f",
+    pass: "c227b35961b0c0"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -15,6 +25,18 @@ app.post('/feedbacks', async (req, res) => {
       screenshot,
     }
   })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Ana Pérola <aperola32@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size:16px; color: #111">`,
+      `<p>Tipo de feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
+  });
 
   return res.status(201).json({data: feedback});
 })
