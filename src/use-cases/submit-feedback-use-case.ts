d7eb48c107ccfd3a713a366f64f0849f0f2f85b5
Ana Perola <anaperolinha@hotmail.com>
2022-05-09 20:40:36 -0300

    Add method sendMail to use-case

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -1,3 +1,4 @@
+import { MailAdapter } from "../adapters/mail-adapter";
 import { FeedbacksRepository } from "../repositories/feedbacks-repository";
 
 interface SubmitFeedbackUseCaseRequest {
@@ -9,6 +10,7 @@ interface SubmitFeedbackUseCaseRequest {
 export class SubmitFeedbackUseCase {
   constructor(
     private feedbacksRepository: FeedbacksRepository,
+    private mailAdapter: MailAdapter,
   ) {}
   
   async execute(request: SubmitFeedbackUseCaseRequest) {
@@ -20,5 +22,14 @@ export class SubmitFeedbackUseCase {
       screenshot
     })
 
+    await this.mailAdapter.sendMail({
+      subject: 'Novo feedback',
+      body: [
+        `<div style="font-family: sans-serif; font-size:16px; color: #111">`,
+        `<p>Tipo de feedback: ${type}</p>`,
+        `<p>Comentário: ${comment}</p>`,
+        `</div>`
+      ].join('\n')
+    })
   }
 }
