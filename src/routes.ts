a7eaeb098c68f5e5d07d382978ab6811b3475b4b
Ana Perola <anaperolinha@hotmail.com>
2022-05-09 20:41:24 -0300

    Removed send mail in route to use-case

--- src/routes.ts
+++ src/routes.ts
@@ -1,24 +1,17 @@
 import express from 'express';
-import nodemailer from 'nodemailer';
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
+import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
 
 export const routes = express.Router();
-
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "472b7d793bcd2f",
-    pass: "c227b35961b0c0"
-  }
-});
 
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
   const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+  const nodemailerMailAdapter = new NodemailerMailAdapter()
   const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbacksRepository
+    prismaFeedbacksRepository,
+    nodemailerMailAdapter
   )
 
   await submitFeedbackUseCase.execute({
@@ -26,18 +19,6 @@ routes.post('/feedbacks', async (req, res) => {
     comment, 
     screenshot
   })
-
-  // await transport.sendMail({
-  //   from: 'Equipe Feedget <oi@feedget.com>',
-  //   to: 'Ana Pérola <aperola32@gmail.com>',
-  //   subject: 'Novo feedback',
-  //   html: [
-  //     `<div style="font-family: sans-serif; font-size:16px; color: #111">`,
-  //     `<p>Tipo de feedback: ${type}</p>`,
-  //     `<p>Comentário: ${comment}</p>`,
-  //     `</div>`
-  //   ].join('\n')
-  // });
 
   return res.status(201).send();
 })
