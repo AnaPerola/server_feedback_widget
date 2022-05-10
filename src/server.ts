f2c49517648f0dca1ddc4909bf1f59220f924f6b
Ana Perola <anaperolinha@hotmail.com>
2022-05-09 21:38:31 -0300

    Add config cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 import { routes } from './routes';
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 app.use(express.json());
 app.use(routes);
