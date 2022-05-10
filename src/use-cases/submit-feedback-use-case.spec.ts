673b59a63126576e200a959efe04dae292e5e0a3
Ana Perola <anaperolinha@hotmail.com>
2022-05-09 21:22:39 -0300

    Add test to not send Mail without type

--- src/use-cases/submit-feedback-use-case.spec.ts
+++ src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,15 +1,24 @@
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case"
 
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: async () => {}},
+  { sendMail: async () => {}}
+)
+
 describe('Submit feedback', () => {
   it('should be able to submit a feedback', async () => {
-    const submitFeedback = new SubmitFeedbackUseCase(
-      { create: async () => {}},
-      { sendMail: async () => {}}
-    )
     await expect(submitFeedback.execute({
       type: 'BUG',
       comment: 'example comment',
       screenshot: 'data:image/png;base64/anateste',
     })).resolves.not.toThrow();
+  })
+
+  it('should not be able to submit a feedback without type', async () => {
+    await expect(submitFeedback.execute({
+      type: '',
+      comment: 'example comment',
+      screenshot: 'data:image/png;base64/anateste',
+    })).rejects.toThrow();
   })
 }) 
